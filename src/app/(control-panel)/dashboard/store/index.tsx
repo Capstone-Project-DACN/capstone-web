d29b69cb8b124ad3bd6da40e4f5c0d376ef2bf06
tranminhhieu212 <minhhieu8112003@gmail.com>
2025-04-14 21:22:16 +0700

    main: dashboardSlice

--- src/app/(control-panel)/dashboard/store/index.tsx
+++ src/app/(control-panel)/dashboard/store/index.tsx
@@ -1,10 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import jobSlice from "./jobSlice";
-import jobDialogSlice from "@/dialogs/job/JobDialogSlice";
+import dashboardSlice from "../store/dashboardSlice";
 
 const reducer = combineReducers({
-  jobSlice,
-  jobDialogSlice
+  dashboardSlice
 });
 
 export default reducer;
